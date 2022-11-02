288adbec97adde5dcb212ee9925f3ce1a28371f8
José López <j3lmiso@gmail.com>
2022-11-02 00:05:34 -0500

    Test

--- src/club-member/club-member.module.ts
+++ src/club-member/club-member.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClubEntity } from 'src/club/club.entity';
-import { MemberEntity } from 'src/member/member.entity'; 
+import { ClubEntity } from '../club/club.entity';
+import { MemberEntity } from '../member/member.entity'; 
 import { ClubMemberService } from './club-member.service';
 import { ClubMemberController } from './club-member.controller';
 
